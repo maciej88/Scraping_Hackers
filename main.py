678a1f948e46fd787f98b340ed8eebe960ed40e6
maciej <mkrywult88@gmail.com>
2022-09-16 19:01:24 +0200

    scrapped title and link

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ from bs4 import BeautifulSoup
 res = requests.get('https://news.ycombinator.com/newest')
 soup = BeautifulSoup(res.text, 'html.parser')
 links = soup.select('.titlelink')
-votes = soup.select('.score')
+subtext = soup.select('.subtext')
 
-def create_custom_hn(links, votes):
+def create_custom_hn(links, subtext):
     hn = []
     for idx, item in enumerate(links):
         title = links[idx].getText()
         href = links[idx].get('href', None)
-        hn.append({'title': title, 'link': href})
+        vote = subtext[idx].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace(' points', ''))
+            hn.append({'title': title, 'link': href, 'votes': points})
     return hn
 
-print(create_custom_hn(links, votes))
+print(create_custom_hn(links, subtext))
