bd12684f577d8e45bea07d2f9f05eb0848332f79
maciej <mkrywult88@gmail.com>
2022-09-18 15:30:29 +0200

    code format

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ soup = BeautifulSoup(res.text, 'html.parser')
 links = soup.select('.titlelink')
 subtext = soup.select('.subtext')
 
+
 def sort_stories_by_votes(hnlist):
-    return sorted(hnlist, key= lambda k:k['votes'], reverse=True )
+    return sorted(hnlist, key=lambda k: k['votes'], reverse=True)
+
+
 def create_custom_hn(links, subtext):
     hn = []
     for idx, item in enumerate(links):
@@ -16,8 +19,9 @@ def create_custom_hn(links, subtext):
         href = item.get('href', None)
         vote = subtext[idx].select('.score')
         if len(vote):
-            points = (vote[0].getText().replace(' point', '').strip('s'))
+            points = int(vote[0].getText().replace(' point', '').strip('s'))
             hn.append({'title': title, 'link': href, 'votes': points})
     return sort_stories_by_votes(hn)
+
 
 pprint.pprint(create_custom_hn(links, subtext))
